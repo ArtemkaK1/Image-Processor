56bc472dedb88a0a2e173d3131dbd775b0aca4aa
Artemiy Kirillov <amkirillov_1@edu.hse.ru>
2022-03-23 23:59:01 +0300

    crop filter fix

--- filters/crop.cpp
+++ filters/crop.cpp
@@ -4,9 +4,9 @@ Image Crop::Apply(Image &image, std::vector<std::string> params) {
     int new_width = std::min(static_cast<int>(std::stoi(params[0])), image.GetWidth());
     int new_height = std::min(static_cast<int>(std::stoi(params[1])), image.GetHeight());
     Image result = Image(new_width, new_height);
-    for (int y = 0; y < result.GetHeight(); ++y) {
+    for (int y = image.GetHeight() - result.GetHeight(); y < image.GetHeight(); ++y) {
         for (int x = 0; x < result.GetWidth(); ++x) {
-            result.GetColor(x, y) = image.GetColor(x, y);
+            result.GetColor(x, y - image.GetHeight() + result.GetHeight()) = image.GetColor(x, y);
         }
     }
     return result;
